c5f292a19047a4a76500ab57d2da42ef84dea753
Isabela Meyer <isabela.demf@gmail.com>
2021-09-06 16:46:27 -0300

    Fix api error when tags is nil.

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -5,10 +5,19 @@ class Api::V1::TicketsController < Api::V1::BaseController
     @tags = params[:tags]
 
     if ticket_params? && tags_params?
-      if create_ticket && create_tag
-        render json: { success: "Your ticket #{@ticket.title} was created!" }, status: 200
+      case @tags
+      when nil
+        if create_ticket.save
+          render json: { success: "Your ticket #{@ticket.title} was created!" }, status: 200
+        else
+          render json: { error: 'Something went wrong processing your request. Please try again.' }, status: 400
+        end
       else
-        render json: { error: 'Something went wrong processing your request. Please try again.' }, status: 400
+        if create_ticket.save! && create_tag.save!
+          render json: { success: "Your ticket #{@ticket.title} was created!" }, status: 200
+        else
+          render json: { error: 'Something went wrong processing your request. Please try again.' }, status: 400
+        end
       end
     else
       render json: { error: 'Your request should have user_id, title and can only include max of 4 tags.' }, status: 422
@@ -19,13 +28,11 @@ class Api::V1::TicketsController < Api::V1::BaseController
 
   def create_ticket
     @ticket = Ticket.new(user_id: @user_id, title: @title)
-    @ticket.save
   end
 
   def create_tag
     tags_count = @tags.count
-    tag = Tag.new(ticket: @ticket, count: tags_count)
-    tag.save
+    @tag = Tag.new(ticket: @ticket, count: tags_count)
   end
 
   def ticket_params?
